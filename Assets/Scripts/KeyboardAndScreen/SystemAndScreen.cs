e76b8fe50e354eedb4600025f89cdfe0dc9b4b1c
SCLKriss <117286300+SCLKskellka@users.noreply.github.com>
2025-04-02 17:01:29 +0200

    feat: finshed puzzle and replaced circular corridor

--- Assets/Scripts/KeyboardAndScreen/SystemAndScreen.cs
+++ Assets/Scripts/KeyboardAndScreen/SystemAndScreen.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace KeyboardAndScreen
 {
@@ -10,13 +11,23 @@ namespace KeyboardAndScreen
         public bool IsUnlock;
         
         [SerializeField] private TMP_Text _display;
+        [SerializeField] private Image _displayBackground;
         [SerializeField] private string _keyInputs;
         
         private string _listInput = "" ;
+        private float _timerError = 0.5f;
+        private bool _isTimer;
 
         private void Start()
         {
             Display();
+            _displayBackground.color = Color.white;
+        }
+
+        private void Update()
+        {
+            if (_isTimer)
+                Timer();
         }
 
         private void Validation()
@@ -24,12 +35,29 @@ namespace KeyboardAndScreen
             if (_listInput == _keyInputs)
             {
                 IsUnlock = true;
+                _displayBackground.color = Color.green;
             }
             else
             {
                 IsUnlock = false;
                 _listInput = "";
                 Display();
+                _isTimer = true;
+            }
+        }
+
+        private void Timer()
+        {
+            _timerError -= Time.deltaTime;
+            if (_timerError <= 0)
+            {
+                _displayBackground.color = Color.white;
+            }
+            else
+            {
+                _displayBackground.color = Color.red;
+                _isTimer = false;
+                _timerError = 0.5f;
             }
         }
 
