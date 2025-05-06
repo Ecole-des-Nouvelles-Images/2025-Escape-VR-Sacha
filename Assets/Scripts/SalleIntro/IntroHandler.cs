41971d3b878fdf5e2d1a0f88a627ec68c82d55f0
Elias Prevost <e.prevost@ensi.lan>
2025-05-06 18:15:41 +0200

    Iniated_IntroRoomScripts

--- Assets/Scripts/SalleIntro/IntroHandler.cs
+++ Assets/Scripts/SalleIntro/IntroHandler.cs
@@ -6,74 +6,9 @@ namespace SalleIntro
 {
     public class IntroHandler : Puzzle
     {
-        [FormerlySerializedAs("playerCamera")] [SerializeField] private Transform _playerCamera;
-        private bool _hasMovedToTarget = false;
-
-        [FormerlySerializedAs("rayDistance")] [SerializeField] private float _rayDistance = 10f;
-        [FormerlySerializedAs("lookLayerMask")] [SerializeField] private LayerMask _lookLayerMask;
-        private bool[] _hasLookedAtTarget;
-        private bool _hasLookedAtAllTargets;
-        private int _totalLookTargets;
-
-        [FormerlySerializedAs("puzzleTable")] [SerializeField] private GameObject _puzzleTable;
-        private bool _puzzleActivated = false;
-
-        private void Start()
-        {
-            _totalLookTargets = LayerMask.LayerToName(_lookLayerMask.value) == "" ? 3 : _lookLayerMask.value;
-            _hasLookedAtTarget = new bool[_totalLookTargets];
-            _puzzleTable.SetActive(false);
-        }
-
-        private void Update()
-        {
-            if (_hasMovedToTarget && !_puzzleActivated && !_hasLookedAtAllTargets)
-            {
-                CheckLookAround();
-            }
-        }
-
         public void PlayerMovedToTarget()
         {
-            _hasMovedToTarget = true;
-        }
-
-        private void CheckLookAround()
-        {
-            Ray ray = new Ray(_playerCamera.position, _playerCamera.forward);
-            if (Physics.Raycast(ray, out RaycastHit hitInfo, _rayDistance, _lookLayerMask))
-            {
-                ScreenComponent target = hitInfo.collider.GetComponent<ScreenComponent>();
-                if (target != null && !_hasLookedAtTarget[target.TargetIndex])
-                {
-                    _hasLookedAtTarget[target.TargetIndex] = true;
-                    Debug.Log($"Regardé l'objet {target.TargetIndex} !");
-                }
-            }
-
-            if (AllLookTargetsCompleted())
-            {
-                _hasLookedAtAllTargets = true;
-                ActivatePuzzle();
-            }
-        }
-
-        private bool AllLookTargetsCompleted()
-        {
-            foreach (bool looked in _hasLookedAtTarget)
-            {
-                if (!looked) return false;
-            }
-            return true;
-        }
-
-        private void ActivatePuzzle()
-        {
-            _puzzleActivated = true;
-            _puzzleTable.SetActive(true);
-            Debug.Log("Puzzle activé !");
             
-            //a faire : logique du puzzle, semblable au rail salle 1.
         }
     }
 }
--- Assets/Scripts/Utils/GameEvents.cs
+++ Assets/Scripts/Utils/GameEvents.cs
@@ -12,5 +12,12 @@ namespace Utils
         public static Action<bool> OnPortalTriggered;
         public static Action OnCandleInside;
         public static Action OnCandleOutside;
+        
+        //elias
+        public static Action OnOpenDrawer;
+        public static Action OnOpenTrap;
+        public static Action OnOpenTeddy;
+        public static Action OnUnlockFinalChest;
+        
     }
 }
