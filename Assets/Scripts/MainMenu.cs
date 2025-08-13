8eaf3f1b4da16424ee4c8a494c2fc630b0f911b1
SCLKriss <c.barrera@nouvellesimages.xyz>
2025-08-13 09:27:00 +0200

    chore: modified all sounds settings to optimize

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,39 +1,20 @@
 using System;
 using System.Collections;
+using Manager;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject _loadingScreen;
-    [SerializeField] private Slider _loadingBarFill;
-
-    private void Awake()
-    {
-        _loadingScreen.SetActive(false);
-    }
 
     public void StartGame()
     {
-        StartCoroutine(LoadSceneAsync("Integration"));
+        SceneLoader.OnLoad.Invoke("Integration");
     }
 
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    IEnumerator LoadSceneAsync(string sceneName)
-    {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
-        _loadingScreen.SetActive(true);
-        while (asyncLoad != null && !asyncLoad.isDone)
-        {
-            Debug.Log(asyncLoad.progress);
-            float progress = Mathf.Clamp01(asyncLoad.progress / .9f);
-            _loadingBarFill.value = progress;
-            yield return new WaitForEndOfFrame();
-        }
     }
 }
