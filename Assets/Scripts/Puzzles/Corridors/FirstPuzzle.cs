701999bd51c2dffa9dbb36b3e1ff76ad6bad4a83
SCLKriss <c.barrera@nouvellesimages.xyz>
2025-09-01 12:28:33 +0200

    chore: modified indications in corridor2 + some lights adjustments

--- Assets/Scripts/Puzzles/Corridors/FirstPuzzle.cs
+++ Assets/Scripts/Puzzles/Corridors/FirstPuzzle.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -5,15 +6,36 @@ namespace Puzzles.Corridors
 {
     public class FirstPuzzle : Puzzle
     {
-        [FormerlySerializedAs("_unlockDetector")] [SerializeField] private ObjectDetectorByTag _unlockDetectorByTag;
-        [FormerlySerializedAs("_lockDetector")] [SerializeField] private ObjectDetectorByTag _lockDetectorByTag;
+        [SerializeField] private ObjectDetectorByTag _unlockDetectorByTag;
+        [SerializeField] private ObjectDetectorByTag _lockDetectorByTag;
+        [SerializeField] private GameObject _goodPanels;
+        [SerializeField] private GameObject _badPanels;
+
+        private int _pathCounter;
+
+        private void Awake()
+        {
+            _goodPanels.SetActive(false);
+            _badPanels.SetActive(true);
+        }
 
         private void Update()
         {
             if(_lockDetectorByTag.ObjectDetected)
+            {
                 LockPortal();
+                _pathCounter++;
+            }
             if(_unlockDetectorByTag.ObjectDetected)
+            {
                 UnlockPortal();
+                _pathCounter++;
+            }
+            if (_pathCounter >= 6 && !_goodPanels.activeSelf)
+            {
+                _badPanels.SetActive(false);
+                _goodPanels.SetActive(true);
+            }
         }
     }
 }
