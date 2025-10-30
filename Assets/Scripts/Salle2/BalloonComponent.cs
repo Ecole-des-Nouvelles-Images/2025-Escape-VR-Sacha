9a049fec9562c223d27f68261c3acaab041bb14f
Elias_Prevost <e.prevost@nouvellesimages.xyz>
2025-10-30 16:14:13 +0100

    BalloonSetup_working

--- Assets/Scripts/Salle2/BalloonComponent.cs
+++ Assets/Scripts/Salle2/BalloonComponent.cs
@@ -1,28 +1,37 @@
-using System;
-using System.Runtime.CompilerServices;
 using UnityEngine;
+using System.Collections;
 
 namespace Salle2
 {
     public class BalloonComponent : MonoBehaviour
     {
         private Rigidbody _rigidbody;
-        [SerializeField] private int _ballonCode;
+        private Collider _collider;
+        public string BallonCode;
+        private LivingroomHandler _livingroomHandler;
 
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
+            _collider = GetComponent<Collider>();
+            _livingroomHandler = FindObjectOfType<LivingroomHandler>();
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void OnCollisionEnter(Collision other)
         {
-            if (other.gameObject.CompareTag("Hand"))
+            if (other.gameObject.CompareTag("Hands"))
             {
-                _rigidbody.AddForce(0, 10, 0, ForceMode.Impulse);
+                _rigidbody.AddForce(Vector3.up * 0.5f, ForceMode.Impulse);
+                _livingroomHandler.BallonTouched(BallonCode);
+                StartCoroutine(DisableColliderForSeconds(0.3f));
             }
         }
-        
-        
-        
+
+        private IEnumerator DisableColliderForSeconds(float seconds)
+        {
+            _collider.enabled = false;
+            yield return new WaitForSeconds(seconds);
+            _collider.enabled = true;
+        }
     }
 }
--- Assets/Scripts/Salle2/LivingroomHandler.cs
+++ Assets/Scripts/Salle2/LivingroomHandler.cs
@@ -1,19 +1,31 @@
 using Puzzles;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Salle2
 {
     public class LivingroomHandler : Puzzle
     {
-        [SerializeField] private GameObject[] _emplacements;
-        [SerializeField] private GameObject _lettre;
-        
-        
-        
-        public void BallonTouched(int ballonCode)
+        [SerializeField] private GameObject[] _balloons;
+        private string _code = "12345";
+        private List<string> _currentInput = new List<string>();
+
+        public void BallonTouched(string ballonCode)
         {
-        Debug.unityLogger.Log("BallonTouched", ballonCode);
+            Debug.Log("Ballon touch : " + ballonCode);
+            _currentInput.Add(ballonCode);
+
+            if (_currentInput.Count > _code.Length || _currentInput[_currentInput.Count - 1] != _code[_currentInput.Count - 1].ToString())
+            {
+                _currentInput.Clear();
+                Debug.Log("wrong code");
+            }
+
+            if (_currentInput.Count == _code.Length && string.Join("", _currentInput) == _code)
+            {
+                Debug.Log("correct code, opening portal");
+                UnlockPortal();
+            }
         }
-        
     }
 }
