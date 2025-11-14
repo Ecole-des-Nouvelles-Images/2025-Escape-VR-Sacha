4c6176639f5dc6ca537df51da6f771cd54318b61
Christopher Barrera <c.barrera@ensi.lan>
2025-11-14 13:43:45 +0100

    fix: finished numpad keys clamp + refered missing ref

--- Assets/Scripts/KeyboardAndScreen/DisplacementClamp.cs
+++ Assets/Scripts/KeyboardAndScreen/DisplacementClamp.cs
@@ -5,67 +5,98 @@ namespace KeyboardAndScreen
 {
     public class DisplacementClamp : MonoBehaviour
     {
+        public enum Axes
+        {
+            X,
+            Y,
+            Z
+        }
+        
+        [SerializeField] private Axes _axes = Axes.X;
         [SerializeField] private float _maxPushDisplacement = 0.5f;
         [SerializeField] private bool _moveForward = true;
         
-        private NumepadKeyPhysicControler _physicController;
         private Vector3 _originalPosition;
+        private Vector3 _newPos;
 
         private void Awake()
         {
             _originalPosition = transform.position;
-            _physicController = GetComponent<NumepadKeyPhysicControler>();
         }
 
         private void Update()
         {
-            switch (_physicController.axes)
+            switch (_axes)
             {
-                case NumepadKeyPhysicControler.Axes.X:
+                case Axes.X:
                     if (_moveForward)
                     {
-                        if (transform.position.x > _maxPushDisplacement + _originalPosition.x)
+                        if (transform.position.x > _originalPosition.x +_maxPushDisplacement)
                         {
-                            Vector3 newPos = new Vector3(_maxPushDisplacement + _originalPosition.x, transform.position.y, transform.position.z) ;
+                            _newPos = new Vector3(_originalPosition.x +_maxPushDisplacement, transform.position.y, transform.position.z);
+                        }
+                        else if (transform.position.x < _originalPosition.x )
+                        {
+                            _newPos = new Vector3(_originalPosition.x, transform.position.y, transform.position.z);
                         }
                     }
                     else 
                     {
-                        if (transform.position.x < _maxPushDisplacement - _originalPosition.x)
+                        if (transform.position.x < _originalPosition.x - _maxPushDisplacement)
                         {
-                            
+                            _newPos = new Vector3(_originalPosition.x - _maxPushDisplacement, transform.position.y, transform.position.z);
+                        }
+                        else if (transform.position.x > _originalPosition.x )
+                        {
+                            _newPos = new Vector3(_originalPosition.x, transform.position.y, transform.position.z);
                         }
                     }
                     break;
-                case NumepadKeyPhysicControler.Axes.Y:
+                case Axes.Y:
                     if (_moveForward)
                     {
-                        if (transform.position.y > _maxPushDisplacement + _originalPosition.y)
+                        if (transform.position.y > _originalPosition.y + _maxPushDisplacement)
                         {
-                            
+                            _newPos = new Vector3(transform.position.x, _originalPosition.y + _maxPushDisplacement, transform.position.z);
+                        }
+                        else if (transform.position.y < _originalPosition.y )
+                        {
+                            _newPos = new Vector3(transform.position.x, _originalPosition.y, transform.position.z);
                         }
                     }
                     else 
                     {
-                        if (transform.position.y < _maxPushDisplacement - _originalPosition.y)
+                        if (transform.position.y < _originalPosition.y - _maxPushDisplacement)
                         {
-                            
+                            _newPos = new Vector3(transform.position.x, _originalPosition.y - _maxPushDisplacement, transform.position.z);
+                        }
+                        else if (transform.position.y > _originalPosition.y )
+                        {
+                            _newPos = new Vector3(transform.position.x, _originalPosition.y, transform.position.z);
                         }
                     }
                     break;
-                case NumepadKeyPhysicControler.Axes.Z:
+                case Axes.Z:
                     if (_moveForward)
                     {
-                        if (transform.position.z > _maxPushDisplacement + _originalPosition.z)
+                        if (transform.position.z > _originalPosition.z + _maxPushDisplacement)
                         {
-                            
+                            _newPos = new Vector3(transform.position.x, transform.position.y, _originalPosition.z + _maxPushDisplacement);
+                        }
+                        else if (transform.position.z < _originalPosition.z )
+                        {
+                            _newPos = new Vector3(transform.position.x, transform.position.y, _originalPosition.z);
                         }
                     }
                     else 
                     {
-                        if (transform.position.x < _maxPushDisplacement - _originalPosition.x)
+                        if (transform.position.x < _originalPosition.z - _maxPushDisplacement)
                         {
-                            
+                            _newPos = new Vector3(transform.position.x, transform.position.y, _originalPosition.z - _maxPushDisplacement);
+                        }
+                        else if (transform.position.z > _originalPosition.z )
+                        {
+                            _newPos = new Vector3(transform.position.x, transform.position.y, _originalPosition.z);
                         }
                     }
                     break;
